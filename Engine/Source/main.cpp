231c4c10d68435d141c67ec47a8811e786f761ea
Boomstick64 <71123964+Boomstick64@users.noreply.github.com>
2025-03-29 18:13:11 +1030

    Started working on displaying a window
    - Started following the Microsoft documentation on how to create a window in C++ on windows

--- Engine/Source/main.cpp
+++ Engine/Source/main.cpp
@@ -1,7 +1,49 @@
 #pragma once
+#include <windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int main()
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR lpCmdLine, _In_ int nCmdShow)
+{
+	// Register the window class
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window
+
+	HWND hwnd = CreateWindowEx(
+		0,								// Optional window styles
+		CLASS_NAME,						// Window class
+		L"InfinityEngine",				// Window text
+		WS_OVERLAPPEDWINDOW,			// Window style
+
+		// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+		NULL,		// Parent window
+		NULL,		// Menu
+		hInstance,	// Instance handle
+		NULL		// Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	return 0;
 }
