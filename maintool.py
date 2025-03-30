2ca329b8917dac275d7a5385d287087f865c539b
Boomstick64 <thomastidy@gmail.com>
2025-03-30 12:46:47 +1030

    Started working on Linux support
    - Added Linux Premake
    
    - Started changing maintool.py to account for the current platform

--- maintool.py
+++ maintool.py
@@ -1,7 +1,14 @@
 import os
 import sys
+import platform
 import subprocess
 
 if __name__ == "__main__":
-    # Call premake
-    subprocess.run(("premake5.exe", "vs2022"))
+    print(platform.system())
+
+    if platform.system() == "Windows":
+        # Call premake
+        subprocess.run(("premake5.exe", "vs2022"))
+    elif platform.system() == "Linux":
+        # Call premake
+        subprocess.run(("premake5"))
